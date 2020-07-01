ddc9851df9d911b4e8871791ccf0848de9d1f7bd
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-01 17:29:38 +0530

    create varibles

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let sectionList = document.querySelectorAll("section");
+let navListTag = document.getElementById("navbar_list");
+let sectionLength = sectionList.length;
+let sectionPositions = [];
+let oldPosition = 0;
+let currentPosition = 0;
